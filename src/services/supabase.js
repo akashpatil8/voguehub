503b9a504984bb04b4eef8105a517ccb5bb473d5
Akash Patil <akashpatil4659@gmail.com>
2025-01-02 18:22:36 +0530

    update .env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://jzdyoiwvkpfixjllhfyg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6ZHlvaXd2a3BmaXhqbGxoZnlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwNDE1MDYsImV4cCI6MjAyNjYxNzUwNn0.LZoSZ-azzNuJwYLSuRFrVCkQ5uZgo_Ciy9HA0V5rfxQ";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
